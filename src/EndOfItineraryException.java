df5e9d86d464aca021b4a0865daeccec314a8058
Brzotto <brzotto@gmail.com>
2016-09-27 21:38:10 -0400

    cse214 code
    hw2

---
+++ src/EndOfItineraryException.java
@@ -0,0 +1,15 @@
+
+// 
+/**
+ * The Class EndOfItineraryException.
+ */
+public class EndOfItineraryException extends Exception {
+	
+	/**
+	 * Instantiates a new end of itinerary exception.
+	 */
+	public EndOfItineraryException()
+	{
+		super();
+	}
+}
